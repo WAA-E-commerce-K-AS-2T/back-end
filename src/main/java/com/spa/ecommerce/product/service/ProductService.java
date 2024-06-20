108c140d6a40b3bdb2b65fb6226372a9cbdeeef1
tanushaayer <tanushastan@gmail.com>
2024-06-20 00:00:06 -0500

    product-review

--- src/main/java/com/spa/ecommerce/product/service/ProductService.java
+++ src/main/java/com/spa/ecommerce/product/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.spa.ecommerce.product.service;
 
 import com.spa.ecommerce.product.dto.ProductDTO;
+import com.spa.ecommerce.product.dto.ProductResponseDto;
 import com.spa.ecommerce.product.dto.ProductStatusUpdateDTO;
 import com.spa.ecommerce.review.ReviewDTO;
 import org.springframework.data.domain.Page;
@@ -12,21 +13,21 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
-    Optional<ProductDTO> saveProduct(Principal principal, ProductDTO productDTO, MultipartFile [] photos);
+    ProductResponseDto saveProduct(Principal principal, ProductDTO productDTO, MultipartFile [] photos);
 
-    Page<ProductDTO> getAllProducts(Pageable pageable);
+    Page<ProductResponseDto> getAllProducts(Pageable pageable);
 
-    ProductDTO deleteById(Long id);
+    ProductResponseDto deleteById(Long id);
 
-    Optional<ProductDTO> updateProduct(Long id, ProductDTO productDTO, MultipartFile[] photos);
+    ProductResponseDto updateProduct(Long id, ProductDTO productDTO, MultipartFile[] photos);
 
-    Optional<ProductDTO> getProductById(Long id);
+    ProductResponseDto getProductById(Long id);
 
-    Page<ProductDTO> filterProducts(Pageable pageable, List<Long> categories, Double minPrice, Double maxPrice, String brand, Boolean newArrival, String size, String color, String name);
+    Page<ProductResponseDto> filterProducts(Pageable pageable, List<Long> categories, Double minPrice, Double maxPrice, String brand, Boolean newArrival, String size, String color, String name);
 
     public List<ReviewDTO> getReviewsByProductID(Long id);
 
-    public Optional<ProductDTO> setProductStatus(Long id, ProductStatusUpdateDTO status);
+    public ProductResponseDto setProductStatus(Long id, ProductStatusUpdateDTO status);
 
-    public List<ProductDTO> getProductsBySellerId(Principal principal);
+    public List<ProductResponseDto> getProductsBySellerId(Principal principal);
 }
--- src/main/java/com/spa/ecommerce/review/ReviewDTO.java
+++ src/main/java/com/spa/ecommerce/review/ReviewDTO.java
@@ -1,22 +1,19 @@
 package com.spa.ecommerce.review;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.spa.ecommerce.product.dto.ProductDTO;
 import com.spa.ecommerce.product.entity.Product;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class ReviewDTO {
     private Long id;
     private double rating;
     private String comment;
-    private ProductDTO product;
 
-    public ReviewDTO(Long id, double rating, String comment, ProductDTO product) {
-        this.id = id;
-        this.rating = rating;
-        this.comment = comment;
-        this.product = product;
-    }
+    private Long productId;
 }
